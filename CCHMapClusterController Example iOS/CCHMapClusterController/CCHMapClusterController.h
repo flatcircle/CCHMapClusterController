c8ab29e60826b2a822ab73b809e835471a819e6a
Jannie Theron <jannie@flatcircle.io>
2016-11-09 10:33:03 +0200

    carthage framework added

---
+++ CCHMapClusterController Example iOS/CCHMapClusterController/CCHMapClusterController.h
@@ -0,0 +1,19 @@
+//
+//  CCHMapClusterController.h
+//  CCHMapClusterController
+//
+//  Created by Jannie Theron on 2016/11/09.
+//  Copyright © 2016 Claus Höfele. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CCHMapClusterController.
+FOUNDATION_EXPORT double CCHMapClusterControllerVersionNumber;
+
+//! Project version string for CCHMapClusterController.
+FOUNDATION_EXPORT const unsigned char CCHMapClusterControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CCHMapClusterController/PublicHeader.h>
+
+
